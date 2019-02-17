55110733bfff247f2326446c44ac1c386ec07886
unknown <setp.sm1t@mail.ru>
2019-02-18 01:21:11 +0500

    add eslint, prettier

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    title: 'Edit <code>src/App.js</code> and save to reload.',
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>{this.state.title}</p>
           <a
             className="App-link"
             href="https://reactjs.org"
