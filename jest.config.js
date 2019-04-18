95e2b1da3a21a71586e6d57e6d618cfcdbb68002
Sm1t <setp.sm1t@mail.ru>
2019-04-19 03:45:22 +0500

    test: add enzyme-to-json/serializer, ErrorsList test render list

--- jest.config.js
+++ jest.config.js
@@ -1,34 +1,10 @@
 module.exports = {
   moduleFileExtensions: ['js'],
-
-  // A preset that is used as a base for Jest's configuration
-  // preset: null,
-
-  // The root directory that Jest should scan for tests and modules within
-  // rootDir: null,
-
-  // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
-
-  // The test environment that will be used for testing
   testEnvironment: 'node',
 
-  // Options that will be passed to the testEnvironment
-  // testEnvironmentOptions: {},
-
-  // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
-
-  // A map from regular expressions to paths to transformers
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
   },
-  setupFilesAfterEnv: [
-    '<rootDir>/setupTests.js',
-  ],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
 };
--- src/components/ErrorsList.test.js
+++ src/components/ErrorsList.test.js
@@ -3,9 +3,17 @@ import { shallow } from 'enzyme';
 
 import ErrorsList from './ErrorsList';
 
-describe('Login', () => {
+describe('ErrorsList', () => {
   it('should render without problems', () => {
     const errorsList = shallow(<ErrorsList />);
+
+    expect(errorsList).toMatchSnapshot();
+  });
+
+  it('should properly render errors list', () => {
+    const errorsList = shallow(
+      <ErrorsList errors={['error one', 'error two']} />,
+    );
 
     expect(errorsList).toMatchSnapshot();
   });
